3771381f8ffcf665da7fdf0443a6171826299132
NotP3RL <84793664+NotP3RL@users.noreply.github.com>
2021-10-21 15:11:23 +0300

    Упорядочил импорты

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import os
-import requests
-from urllib.parse import urlparse
-from dotenv import load_dotenv
 import argparse
+import os
+from urllib.parse import urlparse
+
+from dotenv import load_dotenv
+import requests
 
 
 def shorten_link(headers, user_url):
